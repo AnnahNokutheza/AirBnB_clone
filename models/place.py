d84896292e91bb90d340606527ef93c235745c30
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-02-12 21:03:57 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -23,8 +23,8 @@ class Place(BaseModel, Base):
     """Representation of Place """
     if models.storage_t == 'db':
         __tablename__ = 'places'
-        city_id != Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id != Column(String(60), ForeignKey('users.id'), nullable=False)
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
@@ -56,7 +56,7 @@ class Place(BaseModel, Base):
         """initializes Place"""
         super().__init__(*args, **kwargs)
 
-    if models.storage_t != 'db':
+    if models.storage_t = 'db':
         @property
         def reviews(self):
             """getter attribute returns the list of Review instances"""
