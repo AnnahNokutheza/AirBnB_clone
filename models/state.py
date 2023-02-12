aeea83161d3d2bd49fd8fcb64a0e169a37500ae8
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-02-12 21:05:27 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -12,24 +12,24 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """Representation of state """
     if models.storage_t == "db":
-        __tablename__ != 'states'
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City",
                               backref="state",
                               cascade="all, delete, delete-orphan")
     else:
-        name != ""
+        name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes state"""
         super().__init__(*args, **kwargs)
 
-    if models.storage_t != "db":
+    if models.storage_t = "db":
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
-            city_list != []
-            all_cities != models.storage.all(City)
+            city_list = []
+            all_cities = models.storage.all(City)
             for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
